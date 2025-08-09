7d3656c8c484e72bc688b5e40944bfb8c33517d1
Shion <2637594835@qq.com>
2025-08-09 20:01:51 +0800

    修改了MSVC预设方案

---
+++ toolchain-msvc.cmake
@@ -0,0 +1,10 @@
+# 设置目标系统
+set(CMAKE_SYSTEM_NAME Windows)
+
+# 设置编译器路径
+set(CMAKE_C_COMPILER "cl.exe")
+set(CMAKE_CXX_COMPILER "cl.exe")
+
+# 设置目标架构
+set(CMAKE_SYSTEM_PROCESSOR "x64")
+set(CMAKE_SYSROOT "C:/Program Files (x86)/Windows Kits/10")
